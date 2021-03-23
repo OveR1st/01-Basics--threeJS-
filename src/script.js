b3e2559502a250a2b2dd22da3fc64d53535813fa
OveR1st <315overdose@gmail.com>
2021-03-23 15:13:20 +0200

    Texture, dat gui, BufferGeometry.

--- src/script.js
+++ src/script.js
@@ -1,11 +1,45 @@
 import * as THREE from 'three';
 import './style.css'
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-// import gsap from 'gsap';
+import gsap from 'gsap';
+import * as dat from 'dat.gui';
+import imageSource from '../static/door.jpg';
+
+// Texture
+const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onStart = () => {
+  console.log('onStart')
+}
+loadingManager.onLoad = () => {
+  console.log('onLoad')
+}
+loadingManager.onProgress = () => {
+  console.log('onProgress')
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const texture = textureLoader.load(imageSource)
+
+// Add Debug
+const gui = new dat.GUI({ closed: true })
+const parameters = {
+  color: 0x86699b,
+  spin: () => {
+    gsap.to(cube1.rotation, { duration: 1, y: cube1.rotation.y + 10 })
+  }
+}
+
+gui
+    .addColor(parameters, 'color')
+    .onChange(() => {
+      cube1.material.color.set(parameters.color)
+    })
+
+gui
+    .add(parameters, 'spin')
 
 const canvas = document.querySelector('.webgl')
-
-console.log(OrbitControls)
 
 // Cursor
 const cursor = {
@@ -38,7 +72,7 @@ window.addEventListener('resize', () => {
 })
 
 window.addEventListener('dblclick', () => {
-  if(!document.fullscreenElement) {
+  if (!document.fullscreenElement) {
     canvas.requestFullscreen()
   } else {
     document.exitFullscreen()
@@ -48,16 +82,45 @@ window.addEventListener('dblclick', () => {
 // scene
 const scene = new THREE.Scene()
 
+//Object
 const group = new THREE.Group()
 scene.add(group)
 
+// const geometry = new THREE.Geometry();
+// const geometry = new THREE.BufferGeometry();
+//
+// const count = 50;
+//
+// const positionsArray = new Float32Array(count * 3 * 3)
+//
+// for (let i = 0; i < positionsArray.length; i++) {
+//   positionsArray[i] = Math.random()
+// }
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+//
+// geometry.setAttribute('position', positionsAttribute)
+
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: '#3dd951'})
+    new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2),
+    new THREE.MeshBasicMaterial({map: texture, wireframe: false})
 )
 group.add(cube1)
 
-cube1.position.set(0, 0, 0)
+// Debug settings
+gui.add(group.position, 'y', -3, 3, 0.01) // min,max,step,name
+gui.add(group.position, 'x', -3, 3, 0.01)
+gui.add(group.position, 'z', -3, 3, 0.01)
+
+gui.add(group, 'visible')
+gui.add(cube1.material, 'wireframe')
+
+gui.add(cube1.scale, 'x', 0, 10, 0.01)
+gui.add(cube1.scale, 'y', 0, 10, 0.01)
+gui.add(cube1.scale, 'z', 0, 10, 0.01)
+console.log(cube1)
+
+
+// cube1.position.set(0, 0, 0)
 
 // const cube2 = new THREE.Mesh(
 //     new THREE.BoxGeometry(1, 1, 1),
@@ -74,8 +137,8 @@ cube1.position.set(0, 0, 0)
 // cube3.position.x = -2
 
 //AxesHelper
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper)
 
 //Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 1000)
@@ -122,7 +185,7 @@ const tick = () => {
 // camera.position.z = Math.cos(cursor.x * Math.PI * 2)*3
 // camera.position.y = cursor.y * 10
   controls.update()
-  camera.lookAt(group.position)
+  // camera.lookAt(group.position)
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
 }
